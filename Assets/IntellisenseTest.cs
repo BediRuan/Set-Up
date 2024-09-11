9e6e3097ed88593aa835a79b778272e2c77a13a4
DESKTOP-C76T5B3\hp <junchenruan92@gmail.com>
2024-09-10 20:29:06 -0700

    Setup

---
+++ Assets/IntellisenseTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntellisenseTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float bediVariable = 0f;
+        bediVariable += 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
